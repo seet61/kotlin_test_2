3c870e530f455242a9c085032166f5daa40cb091
seet61 <seet_seet@mail.ru>
2021-07-26 11:18:40 +0300

    init

---
+++ src/main/kotlin/app.kt
@@ -0,0 +1,19 @@
+fun main() {
+    println("Hello Kotlin")
+
+    //Int variable
+    val age: Int = 23
+    println(age)
+
+    var agge: Int = 25
+    println(agge)
+    agge = 56
+    println(agge)
+
+    val text: String = """
+                        SALT II was a series of talks between United States
+                        and Soviet negotiators from 1972 to 1979.
+                        It was a continuation of the SALT I talks.
+                    """
+    println(text)
+}
